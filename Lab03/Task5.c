b3a349d00d8b9e4dbadaec4c1a250b22e6f01796
k250076-glitch <k250076@nu.edu.pk>
2025-09-13 17:00:40 +0500

    Add files via upload

---
+++ Lab03/Task5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Name: Ali\n");
+    printf("Age: 20\n");
+    printf("Course: \"C Programming\"\n");
+    printf("Result: Passed\n");
+    return 1;
+}
